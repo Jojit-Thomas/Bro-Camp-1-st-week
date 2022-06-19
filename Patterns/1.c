425ab95a13cd94829ff75b25a9053e883c603658
Jojit Thomas <103530443+Jojit-Thomas@users.noreply.github.com>
2022-06-19 16:27:31 +0530

    Update 1.c

--- Patterns/1.c
+++ Patterns/1.c
@@ -1,24 +1,35 @@
+
+//  *
+//* * *
+//  +
+//  +
+//* * * * *
+//  +
+//  +
+//  +
+//* * * * * * *
+//  +
+//  +
+//  +
+//  +
+//* * * * * * * * *
+
 #include<stdio.h>
 
 int main()
 {
-    for(float col = 0; col < 3 ; col+=0.5){
-            printf("* ");
-        }
-
-        printf("\n\n\n\n\n\n");
-
     int limit = 4;
-
-    for(int row = 2; row <= limit; row++){
-        for(int rowStar = 1; rowStar <row  ; rowStar++){
-            printf("  *");
+    int colLimit = 3;
+    for(int row = 1; row <= limit; row++){
+        for(int rowStar = 1; rowStar <= row  ; rowStar++){
+            printf("  +");
             printf("\n");
         }
-
-        for(float col = 1; col <= row ; col+=0.5){
+        
+        for(float col = 1 ; col <= colLimit ; col++){
             printf("* ");
         }
+        colLimit += 2;
         printf("\n");
     }
     
